d533cbf0fe0ac34b9f89da16ac7b0d6885de29ef
See-you-tomorrow <385113031@qq.com>
2017-03-24 08:38:25 +0800

    wode

--- app/src/main/java/com/example/administrator/ichat/MainActivity.java
+++ app/src/main/java/com/example/administrator/ichat/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.administrator.ichat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.hyphenate.EMCallBack;
+import com.hyphenate.chat.EMClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        EMClient.getInstance().login("123","123 ",new EMCallBack() {//回调
+            @Override
+            public void onSuccess() {
+                EMClient.getInstance().groupManager().loadAllGroups();
+                EMClient.getInstance().chatManager().loadAllConversations();
+                Log.d("main", "登录聊天服务器成功！");
+            }
+
+            @Override
+            public void onProgress(int progress, String status) {
+
+            }
+
+            @Override
+            public void onError(int code, String message) {
+                Log.d("main", "登录聊天服务器失败！");
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/administrator/ichat/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.example.administrator.ichat.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/3/22 0022.
+ */
+
+public class BaseActivity extends AppCompatActivity{
+}
